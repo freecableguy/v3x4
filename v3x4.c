8356ad28e47c587c20e2d81165d0f1a9710c4be8
Kris Boughton <32200916+freecableguy@users.noreply.github.com>
2017-11-05 19:46:54 -0700

    v3x4-0.10b-i306f2-rc9

--- v3x4.c
+++ v3x4.c
@@ -5,101 +5,100 @@
 #include <Protocol/MpService.h>
 
 // SVID fixed VCCIN voltages
-#define			_DYNAMIC_SVID			0x0				// FIVR-controlled SVID
-#define			_DEFAULT_SVID			0x0000074D			// no change to default VCCIN
-#define			_VCCIN_1pt600			0x00000667			// 1.600 V
-#define			_VCCIN_1pt625			0x00000680			// 1.625 V
-#define			_VCCIN_1pt650			0x0000069A			// 1.650 V
-#define			_VCCIN_1pt675			0x000006B4			// 1.675 V
-#define			_VCCIN_1pt700			0x000006CD			// 1.700 V
-#define			_VCCIN_1pt725			0x000006E7			// 1.725 V
-#define			_VCCIN_1pt750			0x00000700			// 1.750 V
-#define			_VCCIN_1pt775			0x0000071A			// 1.775 V
-#define			_VCCIN_1pt800			0x00000734			// 1.800 V
-#define			_VCCIN_1pt825			0x0000074D			// 1.825 V (default)
-#define			_VCCIN_1pt850			0x00000767			// 1.850 V
-#define			_VCCIN_1pt875			0x00000780			// 1.875 V
-#define			_VCCIN_1pt900			0x0000079A			// 1.900 V
-#define			_VCCIN_1pt925			0x000007B4			// 1.925 V
-#define			_VCCIN_1pt950			0x000007CD			// 1.950 V
-#define			_VCCIN_1pt975			0x000007E7			// 1.975 V
-#define			_VCCIN_2pt000			0x00000800			// 2.000 V
-#define			_VCCIN_2pt025			0x0000081A			// 2.025 V
-#define			_VCCIN_2pt050			0x00000834			// 2.050 V
-#define			_VCCIN_2pt075			0x0000084D			// 2.075 V
-#define			_VCCIN_2pt100			0x00000867			// 2.100 V
+#define		_DYNAMIC_SVID				0x0				// FIVR-controlled SVID
+#define		_DEFAULT_SVID				0x0000074D			// no change to default VCCIN
+#define		_VCCIN_1pt600				0x00000667			// 1.600 V
+#define		_VCCIN_1pt625				0x00000680			// 1.625 V
+#define		_VCCIN_1pt650				0x0000069A			// 1.650 V
+#define		_VCCIN_1pt675				0x000006B4			// 1.675 V
+#define		_VCCIN_1pt700				0x000006CD			// 1.700 V
+#define		_VCCIN_1pt725				0x000006E7			// 1.725 V
+#define		_VCCIN_1pt750				0x00000700			// 1.750 V
+#define		_VCCIN_1pt775				0x0000071A			// 1.775 V
+#define		_VCCIN_1pt800				0x00000734			// 1.800 V
+#define		_VCCIN_1pt825				0x0000074D			// 1.825 V (default)
+#define		_VCCIN_1pt850				0x00000767			// 1.850 V
+#define		_VCCIN_1pt875				0x00000780			// 1.875 V
+#define		_VCCIN_1pt900				0x0000079A			// 1.900 V
+#define		_VCCIN_1pt925				0x000007B4			// 1.925 V
+#define		_VCCIN_1pt950				0x000007CD			// 1.950 V
+#define		_VCCIN_1pt975				0x000007E7			// 1.975 V
+#define		_VCCIN_2pt000				0x00000800			// 2.000 V
+#define		_VCCIN_2pt025				0x0000081A			// 2.025 V
+#define		_VCCIN_2pt050				0x00000834			// 2.050 V
+#define		_VCCIN_2pt075				0x0000084D			// 2.075 V
+#define		_VCCIN_2pt100				0x00000867			// 2.100 V
 
 // Adaptive negative dynamic voltage offsets for all domains
-#define			_DEFAULT_FVID			0x0				// no change to default VID (Vcore)
-#define			_FVID_MINUS_10_MV		0xFEC00000			// -10 mV  (-0.010 V)
-#define			_FVID_MINUS_20_MV		0xFD800000			// -20 mV  (-0.020 V)
-#define			_FVID_MINUS_30_MV		0xFC200000			// -30 mV  (-0.030 V)
-#define			_FVID_MINUS_40_MV		0xFAE00000			// -40 mV  (-0.040 V)
-#define			_FVID_MINUS_50_MV		0xF9A00000			// -50 mV  (-0.050 V)
-#define			_FVID_MINUS_60_MV		0xF8600000			// -60 mV  (-0.060 V)
-#define			_FVID_MINUS_65_MV		0xF7A00000			// -65 mV  (-0.065 V)
-#define			_FVID_MINUS_70_MV		0xF7000000			// -70 mV  (-0.070 V)
-#define			_FVID_MINUS_71_MV		0xF6E00000			// -71 mV  (-0.071 V)
-#define			_FVID_MINUS_75_MV		0xF6800000			// -75 mV  (-0.075 V)
-#define			_FVID_MINUS_80_MV		0xF5C00000			// -80 mV  (-0.080 V)
-#define			_FVID_MINUS_90_MV		0xF4800000			// -90 mV  (-0.090 V)
-#define			_FVID_MINUS_95_MV		0xF3E00000			// -95 mV  (-0.095 V)
-#define			_FVID_MINUS_100_MV		0xF3400000			// -100 mV (-0.100 V)
-#define			_FVID_MINUS_110_MV		0xF1E00000			// -110 mV (-0.110 V)
-#define			_FVID_MINUS_120_MV		0xF0A00000			// -120 mV (-0.120 V)
-#define			_FVID_MINUS_130_MV		0xEF600000			// -130 mV (-0.130 V)
-#define			_FVID_MINUS_140_MV		0xEE200000			// -140 mV (-0.140 V)
-#define			_FVID_MINUS_150_MV		0xECC00000			// -150 mV (-0.150 V)
+#define		_DEFAULT_FVID				0x0				// no change to default VID (Vcore)
+#define		_FVID_MINUS_10_MV			0xFEC00000			// -10 mV  (-0.010 V)
+#define		_FVID_MINUS_20_MV			0xFD800000			// -20 mV  (-0.020 V)
+#define		_FVID_MINUS_30_MV			0xFC200000			// -30 mV  (-0.030 V)
+#define		_FVID_MINUS_40_MV			0xFAE00000			// -40 mV  (-0.040 V)
+#define		_FVID_MINUS_50_MV			0xF9A00000			// -50 mV  (-0.050 V)
+#define		_FVID_MINUS_60_MV			0xF8600000			// -60 mV  (-0.060 V)
+#define		_FVID_MINUS_65_MV			0xF7A00000			// -65 mV  (-0.065 V)
+#define		_FVID_MINUS_70_MV			0xF7000000			// -70 mV  (-0.070 V)
+#define		_FVID_MINUS_75_MV			0xF6800000			// -75 mV  (-0.075 V)
+#define		_FVID_MINUS_80_MV			0xF5C00000			// -80 mV  (-0.080 V)
+#define		_FVID_MINUS_90_MV			0xF4800000			// -90 mV  (-0.090 V)
+#define		_FVID_MINUS_95_MV			0xF3E00000			// -95 mV  (-0.095 V)
+#define		_FVID_MINUS_100_MV			0xF3400000			// -100 mV (-0.100 V)
+#define		_FVID_MINUS_110_MV			0xF1E00000			// -110 mV (-0.110 V)
+#define		_FVID_MINUS_120_MV			0xF0A00000			// -120 mV (-0.120 V)
+#define		_FVID_MINUS_130_MV			0xEF600000			// -130 mV (-0.130 V)
+#define		_FVID_MINUS_140_MV			0xEE200000			// -140 mV (-0.140 V)
+#define		_FVID_MINUS_150_MV			0xECC00000			// -150 mV (-0.150 V)
 
 // toolbox for MSR OC Mailbox (experimental)
-#define			OC_MB_COMMAND_EXEC		0x8000000000000000
-#define			OC_MB_GET_CPU_CAPS		0x0000000100000000
-#define			OC_MB_GET_TURBO_RATIOS		0x0000000200000000
-#define			OC_MB_GET_FVIDS_RATIOS		0x0000001000000000
-#define			OC_MB_SET_FVIDS_RATIOS		0x0000001100000000
-#define			OC_MB_GET_SVID_PARAMS		0x0000001200000000
-#define			OC_MB_SET_SVID_PARAMS		0x0000001300000000
-#define			OC_MB_GET_FIVR_PARAMS		0x0000001400000000
-#define			OC_MB_SET_FIVR_PARAMS		0x0000001500000000
+#define		OC_MB_COMMAND_EXEC			0x8000000000000000
+#define		OC_MB_GET_CPU_CAPS			0x0000000100000000
+#define		OC_MB_GET_TURBO_RATIOS			0x0000000200000000
+#define		OC_MB_GET_FVIDS_RATIOS			0x0000001000000000
+#define		OC_MB_SET_FVIDS_RATIOS			0x0000001100000000
+#define		OC_MB_GET_SVID_PARAMS			0x0000001200000000
+#define		OC_MB_SET_SVID_PARAMS			0x0000001300000000
+#define		OC_MB_GET_FIVR_PARAMS			0x0000001400000000
+#define		OC_MB_SET_FIVR_PARAMS			0x0000001500000000
 
-#define			OC_MB_DOMAIN_IACORE		0x0000000000000000		// IA Core domain
-#define			OC_MB_DOMAIN_GFX		0x0000010000000000
-#define			OC_MB_DOMAIN_CLR		0x0000020000000000		// CLR (CBo/LLC/Ring) a.k.a. Cache/Uncore domain
-#define			OC_MB_DOMAIN_SA			0x0000030000000000		// System Agent (SA) domain
-#define			OC_MB_DOMAIN_AIO		0x0000040000000000
-#define			OC_MB_DOMAIN_DIO		0x0000050000000000
+#define		OC_MB_DOMAIN_IACORE			0x0000000000000000		// IA Core domain
+#define		OC_MB_DOMAIN_GFX			0x0000010000000000
+#define		OC_MB_DOMAIN_CLR			0x0000020000000000		// CLR (CBo/LLC/Ring) a.k.a. Cache/Uncore domain
+#define		OC_MB_DOMAIN_SA				0x0000030000000000		// System Agent (SA) domain
+#define		OC_MB_DOMAIN_AIO			0x0000040000000000
+#define		OC_MB_DOMAIN_DIO			0x0000050000000000
 
-#define			OC_MB_FIVR_FAULTS_OVRD_EN	0x1				// bit 0
-#define			OC_MB_FIVR_EFF_MODE_OVRD_EN	0x2				// bit 1
-#define			OC_MB_FIVR_DYN_SVID_CONTROL_DIS	0x80000000			// bit 31
+#define		OC_MB_FIVR_FAULTS_OVRD_EN		0x1				// bit 0
+#define		OC_MB_FIVR_EFF_MODE_OVRD_EN		0x2				// bit 1
+#define		OC_MB_FIVR_DYN_SVID_CONTROL_DIS		0x80000000			// bit 31
 
-#define			OC_MB_SUCCESS			0x0
-#define			OC_MB_REBOOT_REQUIRED		0x7
+#define		OC_MB_SUCCESS				0x0
+#define		OC_MB_REBOOT_REQUIRED			0x7
 
 // Model Specific Registers
-#define			MSR_IA32_BIOS_SIGN_ID		0x08B
-#define			MSR_PLATFORM_INFO		0x0CE
-#define			MSR_OC_MAILBOX			0x150
-#define			MSR_FLEX_RATIO			0x194
-#define			MSR_TURBO_RATIO_LIMIT		0x1AD
-#define			MSR_TURBO_RATIO_LIMIT1 		0x1AE
-#define			MSR_TURBO_RATIO_LIMIT2 		0x1AF
-#define			MSR_UNCORE_RATIO_LIMIT		0x620
+#define		MSR_IA32_BIOS_SIGN_ID			0x08B
+#define		MSR_PLATFORM_INFO			0x0CE
+#define		MSR_OC_MAILBOX				0x150
+#define		MSR_FLEX_RATIO				0x194
+#define		MSR_TURBO_RATIO_LIMIT			0x1AD
+#define		MSR_TURBO_RATIO_LIMIT1 			0x1AE
+#define		MSR_TURBO_RATIO_LIMIT2 			0x1AF
+#define		MSR_UNCORE_RATIO_LIMIT			0x620
 
 // constants
-#define			AP_EXEC_TIMEOUT			1000000				// 1 second
-#define			CPUID_VERSION_INFO		0x1
-#define			CPUID_BRAND_STRING_BASE		0x80000002
-#define			CPUID_BRAND_STRING_LEN		0x30
-#define			MSR_FLEX_RATIO_OC_LOCK_BIT	0x100000			// bit 20, set to lock MSR 0x194
-#define			MSR_TURBO_RATIO_SEMAPHORE_BIT	0x8000000000000000		// set to execute changes writen to MSR 0x1AD, 0x1AE, 0x1AF
-#define			MSR_BIOS_NO_UCODE_PATCH		0x0
+#define		AP_EXEC_TIMEOUT				1000000				// 1 second
+#define		CPUID_VERSION_INFO			0x1
+#define		CPUID_BRAND_STRING_BASE			0x80000002
+#define		CPUID_BRAND_STRING_LEN			0x30
+#define		MSR_FLEX_RATIO_OC_LOCK_BIT		0x100000			// bit 20, set to lock MSR 0x194
+#define		MSR_TURBO_RATIO_SEMAPHORE_BIT		0x8000000000000000		// set to execute changes writen to MSR 0x1AD, 0x1AE, 0x1AF
+#define		MSR_BIOS_NO_UCODE_PATCH			0x0
 
 // build options
-#define			BUILD_RELEASE_VER		L"v3x4-0.10b-i306f2-rc9"	// build version
-#define			BUILD_TARGET_CPU_DESC		L"\"Haswell-E/EP\""		// target CPU description (codename)
-#define			BUILD_TARGET_CPUID_SIGN		0x306F2				// target CPUID, set 0xFFFFFFFF to bypass checking
-#define			MAX_PACKAGE_COUNT		4				// maximum number of supported packages/sockets, increase as needed
+#define		BUILD_RELEASE_VER			L"v3x4-0.10b-i306f2-rc9"	// build version
+#define		BUILD_TARGET_CPU_DESC			L"\"Haswell-E/EP\""		// target CPU description (codename)
+#define		BUILD_TARGET_CPUID_SIGN			0x306F2				// target CPUID, set 0xFFFFFFFF to bypass checking
+#define		MAX_PACKAGE_COUNT			4				// maximum number of supported packages/sockets, increase as needed
 
 // driver settings
 const UINTN	CPU_SET_MAX_TURBO_RATIO		=	0;				// 0 for auto max: Core turbo ratio, not to exceed fused limit, no less than MFM
@@ -125,7 +124,7 @@ const UINT32 CLR_ADAPTIVE_OFFSET[MAX_PACKAGE_COUNT] \
 
 // Domain 3 (SA) dynamic voltage offsets per package, adjust as needed
 const UINT32 SA_ADAPTIVE_OFFSET[MAX_PACKAGE_COUNT] \
-	= { _DYNAMIC_FVID, _DYNAMIC_FVID, _DYNAMIC_FVID, v };
+	= { _DYNAMIC_FVID, _DYNAMIC_FVID, _DYNAMIC_FVID, _DYNAMIC_FVID };
 
 // objects
 typedef struct _DOMAIN_OBJECT {
@@ -169,18 +168,18 @@ UINT64				ResponseBuffer;						// general buffer for MSR data reading
 UINT64				ProgramBuffer;						// general buffer for MSR data writing
 
 // function prototypes
-EFI_STATUS  EFIAPI InitializeSystem(IN EFI_SYSTEM_TABLE *SystemTable);
-EFI_STATUS	EFIAPI GatherPlatformInfo(IN OUT PPLATFORM_OBJECT *PlatformObject);
-EFI_STATUS	EFIAPI EnumeratePackages(VOID);
-BOOLEAN		EFIAPI IsValidPackage(VOID);
-VOID		EFIAPI ProgramPackage(IN OUT VOID *Buffer);
-VOID		EFIAPI FinalizeProgramming(VOID);
-VOID		EFIAPI SetCPUConfiguration(VOID);
-VOID		EFIAPI SetFIVRConfiguration(VOID);
+EFI_STATUS  	EFIAPI 		InitializeSystem(IN EFI_SYSTEM_TABLE *SystemTable);
+EFI_STATUS	EFIAPI 		GatherPlatformInfo(IN OUT PPLATFORM_OBJECT *PlatformObject);
+EFI_STATUS	EFIAPI 		EnumeratePackages(VOID);
+BOOLEAN		EFIAPI 		IsValidPackage(VOID);
+VOID		EFIAPI 		ProgramPackage(IN OUT VOID *Buffer);
+VOID		EFIAPI 		FinalizeProgramming(VOID);
+VOID		EFIAPI 		SetCPUConfiguration(VOID);
+VOID		EFIAPI 		SetFIVRConfiguration(VOID);
 
-VOID		EFIAPI OC_MAILBOX_DISPATCH(VOID);
-VOID		EFIAPI OC_MAILBOX_REQUEST(VOID);
-BOOLEAN		EFIAPI OC_MAILBOX_CLEANUP(VOID);
+VOID		EFIAPI 		OC_MAILBOX_DISPATCH(VOID);
+VOID		EFIAPI 		OC_MAILBOX_REQUEST(VOID);
+BOOLEAN		EFIAPI 		OC_MAILBOX_CLEANUP(VOID);
 
 // driver main
 EFI_STATUS
